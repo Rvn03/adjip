a3406fe38bd9ebaf226ce8f725bc58319e60fc99
Rvn03 <117373262+Rvn03@users.noreply.github.com>
2022-11-08 16:15:28 +0500

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,7 @@
+/*=======================typing animation=============================*/
+var typed = new Typed(".typing",{
+    strings:["","КА, ВС, HoReCa, Опт, регионы. ","КА, ВС, HoReCa, Опт, регионы. ","B2B"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+}) 
